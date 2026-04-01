c567471ad99ee2608718c16519da4b6e59097ef6
shubhamkapse11 <shubhamkapse1110@gmail.com>
2026-04-02 00:30:36 +0530

    fixed changes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
 const app = require("./app");
-
+   
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
